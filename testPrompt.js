d1511cc6879970ad643377b18f86bb6e9ec2e3ea
KOSSY <abonekosiso@gmail.com>
2022-06-25 20:27:17 +0100

    added package.json

---
+++ testPrompt.js
@@ -0,0 +1,14 @@
+// const prompt = require('prompt-sync')({sigint: true})
+
+// const arr = []
+
+// const size = prompt('How many elements do you want? ')
+
+// console.log("Start typing in Numbers: ")
+// for(let i = 0; i < size; i++){
+//     const value = prompt('')
+//     arr[i] = value;
+// }
+
+// console.log(arr)
+
