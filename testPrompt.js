30568dd4ba02fb8ad5582a692770e9887b2e32f9
KOSSY <abonekosiso@gmail.com>
2022-06-26 23:31:23 +0100

    rem

--- testPrompt.js
+++ testPrompt.js
@@ -1,14 +1,14 @@
-// const prompt = require('prompt-sync')({sigint: true})
+const prompt = require('prompt-sync')({sigint: true})
 
-// const arr = []
+const arr = []
 
-// const size = prompt('How many elements do you want? ')
+const size = prompt('How many elements do you want? ')
 
-// console.log("Start typing in Numbers: ")
-// for(let i = 0; i < size; i++){
-//     const value = prompt('')
-//     arr[i] = value;
-// }
+console.log("Start typing in Numbers: ")
+for(let i = 0; i < size; i++){
+    const value = prompt('')
+    arr[i] = value;
+}
 
-// console.log(arr)
+console.log(arr)
 
